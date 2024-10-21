7ba8cafd1ff05df4a3685116e5469838bc8f8fbf
Jayadeepkasse <153552048+Jayadeepkasse@users.noreply.github.com>
2024-10-20 21:51:20 -0400

    11

--- Codes/Test.py
+++ Codes/Test.py
@@ -2,6 +2,8 @@
 import pandas as pd
 data=pd.read_csv("/Users/jayadeep/Desktop/Prep/Facebook_EDA/FB_Data.csv")
 print(data.isnull().sum())
+print((data == "").sum())
+print((data == "Unknown").sum())
 
 
 
